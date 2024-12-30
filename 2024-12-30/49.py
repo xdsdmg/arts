2e32ed60b7b3b82ba14822d88cb9cb57ee74a188
xdsdmg <2216336843@qq.com>
2024-12-31 00:00:42 +0800

    wip: 2024-12-30 finish alg

--- 2024-12-30/49.py
+++ 2024-12-30/49.py
@@ -5,22 +5,49 @@ https://leetcode.cn/problems/group-anagrams/?envType=study-plan-v2&envId=top-100
 from typing import List, Dict
 
 
+def str2map(str) -> Dict:
+    res = {}
+
+    for c in str:
+        res[c] = 1 if c not in res else res[c] + 1
+
+    return res
+
+
+def cmp_map(map_1, map_2) -> bool:
+    if len(map_1) != len(map_2):
+        return False
+
+    for k in map_1:
+        if (k not in map_2) or (map_1[k] != map_2[k]):
+            return False
+
+    return True
+
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         records: List[Dict] = []
 
         for s in strs:
+            flag = False
+            m = str2map(s)
+
             for r in records:
-                if len(s) != len(r):
+                if not cmp_map(m, r["map"]):
                     continue
-                for c in s:
-                    if c not in r["map"]:
-                        continue
+
                 r["arr"].append(s)
+                flag = True
+
+            if not flag:
+                records.append({"map": m, "arr": [s]})
 
         return [r["arr"] for r in records]
 
 
 if __name__ == "__main__":
+    strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
     s = Solution()
-    s.groupAnagrams([])
+    res = s.groupAnagrams(strs)
+    print(res)
