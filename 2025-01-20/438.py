5f6d0123d6b976ec2fc754facd8b954f562ec2d9
xdsdmg <2216336843@qq.com>
2025-02-03 00:47:22 +0800

    feat (wip): 2025-01-20

---
+++ 2025-01-20/438.py
@@ -0,0 +1,41 @@
+"""
+https://leetcode.cn/problems/find-all-anagrams-in-a-string/?envType=study-plan-v2&envId=top-100-liked
+"""
+
+from typing import List
+
+
+def to_dict(s: str) -> dict:
+    res = {}
+
+    for c in s:
+        res[c] = 1 if c not in res else res[c] + 1
+
+    return res
+
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        res: List[int] = []
+
+        s_l, p_l = len(s), len(p)
+        d = to_dict(p)
+
+        for i in range(s_l):
+            if i + p_l - 1 > s_l:
+                break
+
+            d_ = to_dict(s[i : i + p_l])
+            if d == d_:
+                res.append(i)
+
+        return res
+
+
+if __name__ == "__main__":
+    s = "cbaebabacd"
+    p = "abc"
+
+    s_ = Solution()
+
+    print(s_.findAnagrams(s, p))
